d2f1b62a297c556e5debded9738f11a9b595184a
coni2k <coni2k@users.noreply.github.com>
2018-03-26 10:43:13 +0200

    0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+### Changelog
+
+**0.2.0**
+
+* Updated to work with Visual Studio 2017 & EF 6.2.0
+
+**0.1.0**
+
+* Initial commit
--- DbContextGeneratorWithCodeFirst/Properties/AssemblyInfo.cs
+++ DbContextGeneratorWithCodeFirst/Properties/AssemblyInfo.cs
@@ -1,16 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DbContextGeneratorWithCodeFirst")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DbContextGeneratorWithCodeFirst")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +18,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f7817ea9-e06a-4172-ac55-4d1fdf3798a2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
